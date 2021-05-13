7c889f9a309c986c5fbf720bf10840aa53f6f2e6
中山　宏一 <nakayama@forefrontier.co.jp>
2021-05-13 18:32:14 +0900

    chatの内容をSuspense lazyで遅延ロード

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,13 +1,9 @@
 /* eslint-disable no-undef */
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import Navivation from '../Components/Navigation'
 import useChat from '../Hooks/useChat'
-import dayjs from 'dayjs'
-import * as ReactMarkdown from 'react-markdown'
-import CodeBlock from '../Components/CodeBlock'
-import gfm from 'remark-gfm'
-import TextareaAutosize from 'react-textarea-autosize';
 
+const ChatBlock = lazy(() => import('../Components/ChatBlock'))
 export default function Chat() {
     const {
         handleRoomChange, handleInputEnter, handleInputChange, handleSoundChange, handleReactionUpdateClick, handleChatEdit, editChatOnChange, handleChatEdited, handleChatDeleted, getRootProps, uploadFile,
@@ -25,50 +21,14 @@ export default function Chat() {
                 {messages && <ul className="messages" id="message">
                     {
                         messages.map((data, idx) => {
-                            if (!data) {
-                                <li key={idx}>
-                                    <div className="pic"></div>
-                                    <div className="txt">--no-data--</div>
-                                </li>
-                            }
-                            const className = (data.userId === userId) ? "left-side" : "right-side"
-                            const imageUrl = (data.userId === userId)
-                                ? "https://s3-ap-northeast-1.amazonaws.com/mable.bucket/comander.png"
-                                : "https://s3-ap-northeast-1.amazonaws.com/mable.bucket/pregident.png"
-                            const time = dayjs(data.createdDatetime).format("M[/]D HH:mm");
-                            const isMime = (data.userId === userId)
-                            const userName = roommates.find(e => e.userId === data.userId)?.userName;
                             return (
-                                <li className={className} key={idx}>
-                                    <div className="pic">
-                                        <img src={imageUrl} alt={data} />
-                                    </div>
-                                    <div className="content">
-                                        <div className="edit_bar">
-                                            <span className="name">{userName}</span>
-                                            <span className="time">{time}</span>
-                                            <button className="edit-button" onClick={(e) => handleChatEdit(e, data.chatId)}
-                                                style={{ display: data.editMode || !isMime ? 'none' : '' }}>📝 :編集</button>
-                                            <button className="edit-button" onClick={(e) => handleChatDeleted(e, data.chatId)}
-                                                style={{ display: data.editMode || !isMime ? 'none' : '' }}>❌ :削除</button>
-                                            <button className="edit-button" onClick={(e) => handleChatEdited(e, data.chatId, roomUserId)}
-                                                style={{ display: data.editMode && isMime ? '' : 'none' }}>決定</button>
-                                        </div>
-                                        <TextareaAutosize className="edit-txt" type="text" value={data.content || ''}
-                                            onChange={(e) => editChatOnChange(e, data.chatId, idx)}
-                                            style={{ display: data.editMode ? '' : 'none' }} />
-                                        <ReactMarkdown className="txt" remarkPlugins={[gfm]} children={data.content} components={CodeBlock} />
-                                        <div className="reaction_bar">
-                                            <button className="c-reaction" onClick={(e) => handleReactionUpdateClick(e, data.chatId, roomUserId, "nice", data.reactions)}>
-                                                👍  {data.reactions?.reduce((p, x) => p + x.nice, 0)}
-                                            </button>
-                                            <button className="c-reaction" onClick={(e) => handleReactionUpdateClick(e, data.chatId, roomUserId, "look", data.reactions)}>
-                                                👀  {data.reactions?.reduce((p, x) => p + x.look, 0)}
-                                            </button>
-                                        </div>
-                                    </div>
-                                </li>
-                            );
+                                <Suspense fallback={<div>Loding...</div>}>
+                                    <ChatBlock data={data} roommates={roommates} userId={userId} roomUserId={roomUserId}
+                                        idx={idx} handleChatEdit={handleChatEdit} handleChatDeleted={handleChatDeleted}
+                                        handleChatEdited={handleChatEdited} editChatOnChange={editChatOnChange}
+                                        handleReactionUpdateClick={handleReactionUpdateClick} />
+                                </Suspense>
+                            )
                         })
                     }
                 </ul>}
---
+++ src/Components/ChatBlock.js
@@ -0,0 +1,57 @@
+import React from "react"
+import TextareaAutosize from 'react-textarea-autosize';
+import * as ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
+import CodeBlock from '../Components/CodeBlock'
+import dayjs from 'dayjs'
+
+const ChatBlock = ({ data, roommates, userId, roomUserId, idx,
+    handleChatEdit, handleChatDeleted, handleChatEdited, editChatOnChange, handleReactionUpdateClick
+}) => {
+    if (!data) {
+        <li key={idx}>
+            <div className="pic"></div>
+            <div className="txt">--no-data--</div>
+        </li>
+    }
+    const className = (data.userId === userId) ? "left-side" : "right-side"
+    const imageUrl = (data.userId === userId)
+        ? "https://s3-ap-northeast-1.amazonaws.com/mable.bucket/comander.png"
+        : "https://s3-ap-northeast-1.amazonaws.com/mable.bucket/pregident.png"
+    const time = dayjs(data.createdDatetime).format("M[/]D HH:mm");
+    const isMime = (data.userId === userId)
+    const userName = roommates.find(e => e.userId === data.userId)?.userName;
+    return (
+        <>
+            <li className={className} key={idx}>
+                <div className="pic">
+                    <img src={imageUrl} alt={data} />
+                </div>
+                <div className="content">
+                    <div className="edit_bar">
+                        <span className="name">{userName}</span>
+                        <span className="time">{time}</span>
+                        <button className="edit-button" onClick={(e) => handleChatEdit(e, data.chatId)}
+                            style={{ display: data.editMode || !isMime ? 'none' : '' }}>📝 :編集</button>
+                        <button className="edit-button" onClick={(e) => handleChatDeleted(e, data.chatId)}
+                            style={{ display: data.editMode || !isMime ? 'none' : '' }}>❌ :削除</button>
+                        <button className="edit-button" onClick={(e) => handleChatEdited(e, data.chatId, roomUserId)}
+                            style={{ display: data.editMode && isMime ? '' : 'none' }}>決定</button>
+                    </div>
+                    <TextareaAutosize className="edit-txt" type="text" value={data.content || ''}
+                        onChange={(e) => editChatOnChange(e, data.chatId, idx)}
+                        style={{ display: data.editMode ? '' : 'none' }} />
+                    <ReactMarkdown className="txt" remarkPlugins={[gfm]} children={data.content} components={CodeBlock} />
+                    <div className="reaction_bar">
+                        <button className="c-reaction" onClick={(e) => handleReactionUpdateClick(e, data.chatId, roomUserId, "nice", data.reactions)}>
+                            👍  {data.reactions?.reduce((p, x) => p + x.nice, 0)}
+                        </button>
+                        <button className="c-reaction" onClick={(e) => handleReactionUpdateClick(e, data.chatId, roomUserId, "look", data.reactions)}>
+                            👀  {data.reactions?.reduce((p, x) => p + x.look, 0)}
+                        </button>
+                    </div>
+                </div>
+            </li>
+        </>)
+}
+export default ChatBlock
